b2c1fe70377cdb099343afd39d24048a73ad1b5e
pantushevdenis <pantushev.denis75@gmail.com>
2024-09-21 19:44:06 +0500

    Латинские числа

--- 003_types/2_dny.py
+++ 003_types/2_dny.py
@@ -1,16 +1,25 @@
-# Отпуск. Пользователь вводит сколько дней осталось до ближайшего отпуска. Программа должна вывести количество выходных дней до отпуска, если учесть? что выходные это суббота и воскресенье, сегодня понедельник и праздники мы не учитываем.
+# Отпуск. Пользователь вводит сколько дней осталось до ближайшего отпуска.
+# Программа должна вывести количество выходных дней до отпуска, если учесть,
+# что выходные это суббота и воскресенье, сегодня понедельник и праздники мы не учитываем.
 # Пример:
 # 4 -> 0
 # 6 -> 1
 # 14 -> 4
 
-days_number_to_vacation = 4
+days_number_to_vacation = 16
 
 assert days_number_to_vacation >= 0
 
-number_sull_weeks, remainder = divmod(days_number_to_vacation, 7)
+NUMBER_WORK_DAYS_IN_WEEK = 5
 
-print(number_sull_weeks, remainder)
+number_full_weeks, remainder = divmod(days_number_to_vacation, 7)
+# print(f"{number_full_weeks} {remainder}")
+number_day_off_of_remainder = remainder - NUMBER_WORK_DAYS_IN_WEEK
+number_days_off = number_full_weeks * 2 + 0 if number_day_off_of_remainder < 0 else number_day_off_of_remainder
+number_working_days = number_full_weeks * 5 + remainder
+
+print(number_days_off)
+
 
 
 
---
+++ 003_types/4_latin.py
@@ -0,0 +1,44 @@
+# Пользователь вводит целое положительное число, программа должна вернуть строку в виде римского числа
+# Пример:
+# 3 -> III
+# 15 -> XV
+# 3 234 -> CCXXXIV
+from builtins import divmod
+
+# input data
+decimal = 436
+
+c, rem_C = divmod(decimal, 100)
+x, i = divmod(rem_C, 10)
+latin = ''
+
+print(c)
+if c == 9:
+    latin = latin + "CM"
+elif c >= 5:
+    latin = latin + "D" + (c - 5) * "C"
+elif c == 4:
+    latin = latin + "CD"
+else:
+    latin = latin + c * "C"
+print(x)
+if x == 9:
+    latin = latin + "XC"
+elif x >= 5:
+    latin = latin + "L" + (x - 5) * "X"
+elif x == 4:
+    latin = latin + "XL"
+else:
+    latin = latin + x * "X"
+print(i)
+if i == 9:
+    latin = latin + "IX"
+elif i >= 5:
+    latin = latin + "V" + (i - 5) * "I"
+elif i == 4:
+    latin = latin + "IV"
+else:
+    latin = latin + i * "I"
+
+#output
+print(latin)
