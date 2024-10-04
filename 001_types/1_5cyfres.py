50b1eb1270d3e6dae17d90efd430254dbcfa8410
pantushevdenis <pantushev.denis75@gmail.com>
2024-10-04 16:34:37 +0500

    Исправлена проверка, добавлен ввод

--- 001_types/1_5cyfres.py
+++ 001_types/1_5cyfres.py
@@ -4,17 +4,15 @@
 # 30789 -> 38709
 from hmac import new
 
-num = 23456
-assert num >= 11111 & num <= 999999
+num = int(input("Введите пятизначное целое число: "))
+assert 10000 <= num <= 999999
 
-print(type(num))
+# print(type(num))
 str_num = str(num)
-print(type(str_num))
+# print(type(str_num))
 central_symbols = str_num[1:4]
 reverted_central_symbols = central_symbols[::-1]
 new_str_num = str_num[0] + reverted_central_symbols + str_num[4]
 new_num = int(new_str_num)
 
 print(new_str_num)
-
-assert new_num == 25436
