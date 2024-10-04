d39bdedda99050c4b0046b68a752101a26179fa8
pantushevdenis <pantushev.denis75@gmail.com>
2024-10-04 22:59:13 +0500

    simple rle со списка на генератор

--- 002_controls/3_simple_rle_2.py
+++ 002_controls/3_simple_rle_2.py
@@ -1,7 +1,7 @@
 import itertools as it
 
 data = "aaabbbbccccc"
-encoding = "".join([ch + str(sum(1 for _ in gr)) for ch, gr in it.groupby(data)])
+encoding = "".join((ch + str(sum(1 for _ in gr)) for ch, gr in it.groupby(data)))
 print(encoding)
 
     
