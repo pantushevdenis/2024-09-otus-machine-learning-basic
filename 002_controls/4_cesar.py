8e921b15e4552d86fa8a49478fce50712943ce39
pantushevdenis <pantushev.denis75@gmail.com>
2024-10-07 21:02:14 +0500

    Шифр Цезаря

---
+++ 002_controls/4_cesar.py
@@ -0,0 +1,30 @@
+# Шифр Цезаря. Пользователь вводит строку и ключ шифра, программа должна вывести зашифрованную строку (со сдвигом по ключу). Сдвиг циклический.
+# Используем только латинский алфавит, пробелы не шифруются.
+# Пример:
+# Dog, 2 -> Fqi
+# Zak zak, 3 -> Cdn cdn
+# Python is the BEST, 5 -> Udymts nx ymj GJXY
+
+str_data = input("Введите строку: ")
+try:
+    k = int(input("Введите ключ: "))
+    assert k > 0
+except:
+    print("Ключ должен быть целым положительным числом")
+    exit()
+
+res_arr = []
+for ch in str_data:
+    a = ord(ch)
+    if 33 <= a <= 122:
+        res = a + k
+        while res > 122:
+            res = 33 + (res - 122) - 1
+        res_arr.append(chr(res))
+    else:
+        res_arr.append(chr(a))
+res_str = "".join(res_arr)
+
+print(res_str)
+
+
